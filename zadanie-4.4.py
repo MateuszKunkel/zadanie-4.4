81a19d757e1fa04f937b78372e12e5c56c0080f9
Mateusz Kunkel <matiqk@wp.pl>
2023-01-26 13:51:32 +0100

    reformat with black

--- zadanie-4.4.py
+++ zadanie-4.4.py
@@ -1,37 +1,44 @@
 import sys
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
+
 
 def calculate(choice, num1, num2):
     if choice == 1:
-        print (f"Wynik to {num1 + num2}")
-        return (num1 + num2)
+        print(f"Wynik to {num1 + num2}")
+        return num1 + num2
     elif choice == 2:
-        print (f"Wynik to {num1 - num2}")
-        return (num1 - num2)
+        print(f"Wynik to {num1 - num2}")
+        return num1 - num2
     elif choice == 3:
-        print (f"Wynik to {num1 * num2}")
-        return (num1 * num2)
+        print(f"Wynik to {num1 * num2}")
+        return num1 * num2
     elif choice == 4:
-        print (f"Wynik to {num1 / num2}")
-        return (num1 / num2)
+        print(f"Wynik to {num1 / num2}")
+        return num1 / num2
     else:
         exit(1)
 
+
 if __name__ == "__main__":
-    choice = int(input("Podaj zamierzone działanie matematyczne, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
+    choice = int(
+        input(
+            "Podaj zamierzone działanie matematyczne, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"
+        )
+    )
     num1 = int(input("podaj pierwszą liczbę:"))
     num2 = int(input("podaj kolejną liczbę:"))
 
     if choice < 1:
         choice = 1
-    if choice > 4:
+    elif choice > 4:
         choice = 4
-    
-    print(choice)
 
-    działania = ['dodaję', 'odejmuję', 'mnożę', 'dzielę']
+    działania = ["dodaję", "odejmuję", "mnożę", "dzielę"]
 
-    logging.debug("zlecona czynność to: %s %s i %s" % (działania[choice - 1], num1, num2))
+    logging.debug(
+        "zlecona czynność to: %s %s i %s" % (działania[choice - 1], num1, num2)
+    )
 
     calculate(choice, num1, num2)
